25f1a718c485c834c5265978738bbdb750c15ff3
Merill Fernando <merill@merill.net>
2022-02-08 22:52:09 +1100

    Initial commit

---
+++ src/GraphPsConverter/GraphPsConverter.Core/Converter.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphPsConverter.Core
+{
+    public class Converter
+    {
+        public string? ConvertToGraphPowerShell(string? sourceScript)
+        {
+            if(sourceScript == null) return null;
+
+            return sourceScript.Replace("get-azureaduser", "get-mguser");
+
+        }
+    }
+}
---
+++ src/GraphPsConverter/GraphPsConverter.Web/Models/ScriptConverterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GraphPsConverter.Web.Models
+{
+    public class ScriptConverterModel
+    {
+        public string? SourceScript { get; set; }
+        public string? ConvertedScript { get; set; }
+    }
+}
