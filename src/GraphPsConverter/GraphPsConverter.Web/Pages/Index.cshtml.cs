b9f3f833ff92c412bca1eebac3c86cae2cab864a
Merill Fernando <merill@merill.net>
2022-04-22 01:09:02 +1000

    Added web page to perform conversion

--- src/GraphPsConverter/GraphPsConverter.Web/Pages/Index.cshtml.cs
+++ src/GraphPsConverter/GraphPsConverter.Web/Pages/Index.cshtml.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GraphPsConverter.Core;
 
 namespace GraphPsConverter.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string SourceScript { get; set; }
+
+        [BindProperty]
+        public string ConvertedScript { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,5 +23,14 @@ namespace GraphPsConverter.Web.Pages
         {
 
         }
+
+        public void OnPostSubmit()
+        {
+            var converter = new Converter();
+            var converted = converter.ConvertToGraphPowerShell(SourceScript);
+
+            ConvertedScript = converted.ConvertedCommands[0].ConvertedScript;
+        }
+
     }
 }
